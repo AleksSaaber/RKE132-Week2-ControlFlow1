c149a556ec49c7cc088d2bd072b44cddbcfb8681
Aleks Saaber <aleks-harald.saaber@tktk.ee>
2024-09-22 13:45:13 +0300

    Add project files.

---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Rakendus küsib kasutajal valida tema sugu (m/f)
+//Rakendus palub kasutajal sisestada tema perekonnanime
+//Lähtudes kasutaja valikust, tervitab rakendus kasutajat järgmiselt
+//"Welcome, Mr. [Kasutaja perekonnanimi]" / "Welcome",Ms. [Kasutaja perekonnanimi]"
+
+Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine()); //Loeb konsoolist maha andmeid string (sõne) formaadis
+Console.WriteLine("Please enter your name");
+
+string userLastName = Console.ReadLine();
+
+if(userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if(userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms, {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
+
